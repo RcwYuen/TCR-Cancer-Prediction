587e9883f8219211a3106d514a26ad7b880d70d0
RcwYuen <zcabcry@ucl.ac.uk>
2024-02-29 00:06:04 +0000

    SCEPTR Reruns

--- utils/makezip.py
+++ utils/makezip.py
@@ -2,14 +2,9 @@ import zipfile
 import glob
 
 paths = [
-    "data/trimmed/**",
-    # "model/mlm-only/model/**",
-    # "model/mlm-only/tokenizer/**",
-    # "scripts/**",
-    # "utils/**",
-    # "trainer.py",
-    # "model.py"
+    "data/sceptr-traintest/**",
 ]
+
 files = []
 for path in paths:
     files = files + list(glob.glob(path, recursive = True))
